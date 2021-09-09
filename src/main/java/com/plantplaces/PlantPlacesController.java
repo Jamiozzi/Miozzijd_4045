e101c10831c8f3ebb00921f30036418b775e6514
Jamiozzi <miozzijd@mail.uc.edu>
2021-09-09 14:40:41 -0400

    Update PlantPlacesController.java
    update endpoints

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,6 +3,10 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/** Handle the /start endpoint
+ * @author Administrator
+ *
+ */
 @Controller
 public class PlantPlacesController {
 
@@ -12,4 +16,13 @@ public class PlantPlacesController {
 			
 			return "start";
 		}
+	
+/** Handle the / endpoint
+ * @author Administrator
+ *
+ */
+public String index() {
+	
+	return "start";
+}
 }
